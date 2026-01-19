e197a0a39b3fb5eae025f2307843c409ca287d61
Gilvan Alves Pastor Junior <gilvan.junior423@gmail.com>
2026-01-19 20:07:31 -0300

    Add 'changedatatype' file

---
+++ changedatatype.py
@@ -0,0 +1,37 @@
+numero_str = "100"
+numero_int = int(numero_str)
+print(numero_int)
+
+#teste_str = "teste"
+#teste_int = int(teste_str)
+#print(teste_int)
+
+numero_str = "100.5"
+numero_float = float(numero_str)
+print(numero_float)
+
+numero_int = 100
+numero_string = str(numero_int)
+print(numero_string)
+
+numero_float = 100.5
+numero_int = int(numero_float)
+print(numero_int)
+
+boolean_str = "TRUE"
+boolean_val = bool(boolean_str)
+print(boolean_val)
+
+numero_int = 1
+boolean_val = bool(numero_int)
+print(boolean_val)
+
+
+#Multipla atribuição:
+
+a, b, c = 1, 2 , 3
+print(a, b, c)
+
+a = b =c = 3
+print(a, b, c)
+
